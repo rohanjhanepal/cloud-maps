635c86dbb852383eff3594338658bfb6e8d8d55d
rohanjhanepal <rohanjhanepal@gmail.com>
2026-02-28 16:01:50 +1100

    Fix base path for cloud-maps

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/cloudgraph-3d/',
+  base: '/cloud-maps/',
 })
